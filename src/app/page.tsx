cae2e5bae4c281818859c97b9c1eacd66ddb13f1
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2024-12-13 23:49:07 +0300

    update library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-'use client'
 
-import { Button } from "./chesty/components/button";
 
 export default function Home() {
   return (
-   <div className="flex justify-center items-center h-screen">
-    <Button asChild={true} variant="default" leftIcon={<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
-<path d="M6 2.5V9.5M2.5 6H9.5" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
-</svg>} ><a href="">fdsfsd</a></Button>
-   </div>
+ <div></div>
   );
 }
